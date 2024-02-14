2374fdd9c2fdd29a9d5182e52457a0ac1abb2aa8
Nazrulofficeial <hmraz101@gmail.com>
2024-02-14 08:51:29 +0600

    maked a game change look

--- scripts/alphaClash.js
+++ scripts/alphaClash.js
@@ -1,1 +1,15 @@
-console.log('Hello Ami asi');
+function play() {
+    // step-1: hide the home screen. to hide the screen add hidden class to the home section
+    const homeSection = document.getElementById('home-screen')
+    homeSection.classList.add('hidden')
+    // console.log(homeSection.classList);
+
+    // step-2: Show the playground
+    const playGroundSection = document.getElementById('play-ground')
+    playGroundSection.classList.remove('hidden');
+    // console.log(playGroundSection.classList);
+}
+
+function continueGAme() {
+    
+}

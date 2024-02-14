f1d3cefb1fbdcaa7af0a07d1751b9d45068c93f5
Nazrulofficeial <hmraz101@gmail.com>
2024-02-14 09:29:21 +0600

    Displayed Random alphabet in screen

--- scripts/alphaClash.js
+++ scripts/alphaClash.js
@@ -1,15 +1,27 @@
-function play() {
-    // step-1: hide the home screen. to hide the screen add hidden class to the home section
-    const homeSection = document.getElementById('home-screen')
-    homeSection.classList.add('hidden')
-    // console.log(homeSection.classList);
+// function play() {
+//     // step-1: hide the home screen. to hide the screen add hidden class to the home section
+//     const homeSection = document.getElementById('home-screen')
+//     homeSection.classList.add('hidden')
+//     // console.log(homeSection.classList);
 
-    // step-2: Show the playground
-    const playGroundSection = document.getElementById('play-ground')
-    playGroundSection.classList.remove('hidden');
-    // console.log(playGroundSection.classList);
+//     // step-2: Show the playground
+//     const playGroundSection = document.getElementById('play-ground')
+//     playGroundSection.classList.remove('hidden');
+//     // console.log(playGroundSection.classList);
+// }
+
+function continueGame() {
+    // step 1: generate a random alphabet
+    const alphabet = getARandomAlphabet();
+    console.log('your random alphabet', alphabet);
+
+    // set randomly generated alphabet to the screen (Show It)
+    const currentAlphabetElement = document.getElementById('current-alphabet')
+    currentAlphabetElement.innerText = alphabet
+
 }
-
-function continueGAme() {
-    
+function play() {
+    hideElementById('home-screen');
+    showElementById('play-ground');
+    continueGame();
 }

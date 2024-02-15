ba1ca6b55021938daa78e8e3ec0535f347d921c8
Nazrulofficeial <hmraz101@gmail.com>
2024-02-15 22:56:59 +0600

    What updated not know!

--- scripts/alphaClash.js
+++ scripts/alphaClash.js
@@ -23,12 +23,35 @@ function handleKeyboardKeyUpEvent(event) {
     // checked match or not
     if (playerPressed === expectedAlphabet) {
         console.log('You get a point');
-        console.log('You have pressed correctly', expectedAlphabet);
+        // update score:
+        // 1. get the current score
+        const currentScoreElement = document.getElementById('current-scores');
+        const currentScoreText = currentScoreElement.innerText;
+        const currentScore = parseInt(currentScoreText)
+        console.log(currentScore);
+
+
+        // 2. increase the score by 1
+        const newScore = currentScore + 1;
+
+
+        // 3. show the updated score
+        currentScoreElement.innerText = newScore;
+        
+        // start a new round
         removeBackgroundColorById(expectedAlphabet)
         continueGame();
     }
     else {
         console.log('You missed, you lost a life');
+        // step 1: get the current life number
+        const currentLifeElement = document.getElementById('current-life');
+        const currentLifeText = currentLifeElement.innerText;
+        const currentLife = parseInt(currentLifeText);
+        // step 2: reduce the life count
+        const newLife = currentLife -1;
+        // step 3: display the updated life count
+        currentLifeElement.innerText = newLife;
     }
 }
 document.addEventListener('keyup', handleKeyboardKeyUpEvent)
